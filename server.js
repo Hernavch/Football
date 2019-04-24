f7395a055d96898533709f5cc70d850a08421751
Hernavch <hernandezvc1292@gmail.com>
2019-04-23 18:30:17 -0700

    npm install

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var express = require("express");
 var mongojs = require("mongojs");
+var axios = require("axios");
+var cheerio = require("cheerio");
+var exphbs = require("express-handlebars");
 
 var app = express();
 
@@ -10,3 +13,32 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
+
+var db = mongojs(databaseUrl, collections);
+db.on("error", function(error) {
+  console.log("Database Error:", error);
+});
+
+axios.get("https://ramsondemand.com/").then(function(response) {
+
+  // Load the HTML into cheerio and save it to a variable
+  // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
+  var $ = cheerio.load(response.data);
+
+  // An empty array to save the data that we'll scrape
+  var results = [];
+
+    $(".nodeTitle").each(function(i, element) {
+
+    var title = $(element).children().text();
+    var link = $(element).find("a").attr("href");
+
+    // Save these results in an object that we'll push into the results array we defined earlier
+    results.push({
+      title: title,
+      link: link
+    });
+});
+
+  console.log(results);
+});
