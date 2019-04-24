74eee2f37e2949728fc2b67bff71a6329994b91f
Hernavch <hernandezvc1292@gmail.com>
2019-04-23 18:06:34 -0700

    inital set up

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var mongojs = require("mongojs");
+
+var app = express();
+
+// Configure our app for morgan and body parsing with express.json and express.urlEncoded
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
