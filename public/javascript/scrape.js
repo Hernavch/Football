a65e8b37e67baa6c8a79be4ad0f9838726572e3a
Hernavch <hernandezvc1292@gmail.com>
2019-04-26 21:44:08 -0700

    server set up

---
+++ public/javascript/scrape.js
@@ -0,0 +1,28 @@
+var db = mongojs(databaseUrl, collections);
+db.on("error", function(error) {
+  console.log("Database Error:", error);
+});
+
+axios.get("https://ramsondemand.com/").then(function(response) {
+
+  // Load the HTML into cheerio and save it to a variable
+  // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
+  var $ = cheerio.load(response.data);
+
+  // An empty array to save the data that we'll scrape
+  var results = [];
+
+    $(".nodeTitle").each(function(i, element) {
+
+    var title = $(element).children().text();
+    var link = $(element).find("a").attr("href");
+
+    // Save these results in an object that we'll push into the results array we defined earlier
+    results.push({
+      title: title,
+      link: link
+    });
+});
+
+  console.log(results);
+});
